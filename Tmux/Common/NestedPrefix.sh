6318c40379ef42525532eab1c937c1cabc4262b8
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-03-25 22:14:15 +0900

    ネストした tmux の操作を調整

--- Tmux/Common/NestedPrefix.sh
+++ Tmux/Common/NestedPrefix.sh
@@ -2,19 +2,27 @@
 
 if [ $1 = 'disable' ] ; then
     nested=$(tmux show-option -v @nested)
-    tmux set-option @nested "N${nested}"
-    tmux set-option prefix None
-    tmux set-option status-style bg=red
-    test "${nested}" = "" || tmux send-keys $2
+    tmux  set-option @nested "N${nested}"
+    tmux  set-option prefix None
+    tmux  set-option status-style  bg=red
+    test "${nested}" = "" || tmux send-keys  $2
+elif [ $1 = 'change' ] ; then
+    nested=$(tmux show-option -v @nested)
+    tmux  set-option @nested "N${nested}"
+    tmux  set-option prefix C-z
+    tmux  bind-key          C-z  send-prefix
+    tmux  set-option status-style  bg=cyan
+    test "${nested}" = "" || tmux send-keys  $2
 else
     nested=$(tmux show-option -v @nested)
     tmux set-option @nested "${nested#N}"
     if test "${nested}" = "N" ; then
-        tmux set-option prefix prefix C-a
-        tmux set-option satus-style bg=white
+        tmux  set-option satus-style  bg=white
+        tmux  unbind-key        C-z
+        tmux  set-option prefix C-a
     else
-        tmux send-keys $2
+        tmux  send-keys  $2
     fi
 fi
 
-set-option -g status-left "[#S]#{?@nested, #{@nested},}"
+# set-option -g status-left "[#S]#{?@nested, #{@nested},}"
